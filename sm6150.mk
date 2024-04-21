4e2b72088e4893b89707e67e93ce635aa1a80672
Edwin Moquete <edwinmmoquete@gmail.com>
2024-04-21 12:11:02 +0530

    sm6150-common: Build android.media.audio.common.types-V2-cpp
    Needed for WFD, and not built default on QPR2
    
    Change-Id: If860613ef4cb6bae7f7a7e72707c24510e36abd6

--- sm6150.mk
+++ sm6150.mk
@@ -558,6 +558,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
+    android.media.audio.common.types-V2-cpp \
     libavservices_minijail_32 \
     libdisplayconfig.qti \
     libnl \
