3e3daa09033a59cd97e270fdf5f5a3c669681623
Nauval Rizky <enuma.alrizky@gmail.com>
2024-05-09 22:58:12 +0545

    sm6150-common: qcom.post_boot: Remove cpuset config This conflicts with init.qcom.power.rc from previous libperfmgr setup
    Signed-off-by: romgharti <romgharti@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -123,13 +123,6 @@ case "$soc_id" in
         done
     done
 
-    # cpuset parameters
-    echo 0-7     > /dev/cpuset/top-app/cpus
-    echo 0-5,7 > /dev/cpuset/foreground/cpus
-    echo 4-5     > /dev/cpuset/background/cpus
-    echo 2-5     > /dev/cpuset/system-background/cpus
-    echo 2-5     > /dev/cpuset/restricted/cpus
-
     # Enable idle state listener
     echo 1 > /sys/class/drm/card0/device/idle_encoder_mask
     echo 100 > /sys/class/drm/card0/device/idle_timeout_ms
