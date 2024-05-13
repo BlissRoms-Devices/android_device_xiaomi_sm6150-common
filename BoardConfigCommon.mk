2c832c4fe51b1287e5dda582532b84d1b7169058
Bruno Martins <bgcngm@gmail.com>
2024-05-13 17:59:23 +0545

    sm6150-common: Use the common device compatibility matrix from QCOM Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720
    Signed-off-by: romgharti <romgharti@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,7 +66,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest_vendor.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
 # Compression
