2f0ac64b5fecf5b1632bb2635d71cf47c961a9d0
romgharti <romgharti@gmail.com>
2024-05-17 11:17:14 +0545

    sm6150-common: Build FM
    Signed-off-by: romgharti <romgharti@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -211,9 +211,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # FM
-# PRODUCT_PACKAGES += \
-#     FM2 \
-#     qcom.fmradio
+ PRODUCT_PACKAGES += \
+     FM2 \
+     qcom.fmradio
 
 #GNSS
 PRODUCT_PACKAGES += \
