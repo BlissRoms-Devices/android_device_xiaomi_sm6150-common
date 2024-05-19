1a0e21015b5491222b6533109c68f246c0f2e0a7
romgharti <romgharti@gmail.com>
2024-05-19 16:46:05 +0545

    sm6150-common: Uprev vendor.qti.hardware.bluetooth_audio to 2.1
    Signed-off-by: romgharti <romgharti@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -88,6 +88,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     bt_stack.conf \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
