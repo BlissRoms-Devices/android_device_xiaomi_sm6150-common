b18521fc3bb01310ccec8050acf856e9d707a2b1
Rajshekar Eashwarappa <reashw@codeaurora.org>
2024-09-02 19:33:55 +0545

    sm6150-common: BoardConfig: Set swiotlb to default size swiotlb size set to 64MB default value.
    Change-Id: Ib256f5d2a1642ba2e635e632e7d1486aa2dd8f13
    Signed-off-by: romgharti <romgharti@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,7 +86,6 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
-BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += kpti=off
 
