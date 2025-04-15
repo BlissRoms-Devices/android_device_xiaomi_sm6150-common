4694c4dd6b4143b3ba647d6bf100a0119aafbe0c
romgharti <romgharti@gmail.com>
2025-04-15 08:03:17 +0545

    sm6150-common: Remove Offending entries Offending entries: libloc_net_iface libloc_net_iface.vendor
    Signed-off-by: romgharti <romgharti@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -293,9 +293,7 @@ PRODUCT_COPY_FILES += \
 
 # Netd
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.1.vendor \
-    libloc_net_iface \
-    libloc_net_iface.vendor
+    android.system.net.netd@1.1.vendor
 
 # Neural Networks
 PRODUCT_PACKAGES += \
