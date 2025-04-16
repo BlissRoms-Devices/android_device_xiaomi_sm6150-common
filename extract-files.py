4e7b55032b83038af1f01f930c77ad4fbbf186f0
romgharti <romgharti@gmail.com>
2025-04-16 08:45:02 +0545

    sm6150-common: Fix vendor.qti.ims.factory ELF checks
    Signed-off-by: romgharti <romgharti@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -42,6 +42,8 @@ lib_fixups: lib_fixups_user_type = {
         'com.qualcomm.qti.dpm.api@1.0',
         'vendor.qti.hardware.fm@1.0',
         'vendor.qti.imsrtpservice@3.0',
+        'vendor.qti.ims.factory@1.0',
+        'vendor.qti.ims.factory@1.1',
     ): lib_fixup_vendor_suffix,
 }
 
