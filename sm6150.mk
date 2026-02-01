5d7a1bcaffbf3b9a9e3d2f9295530970a31f3d7c
LuK1337 <priv.luk@gmail.com>
2026-02-01 09:17:49 +0000

    sm6150-common: Opt out of speaker_layout_channel_mask field Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    Change-Id: I0bf7eafbeb19957326de04ecf7b903580315dc59
    Signed-off-by: romgharti <romgharti@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -31,6 +31,8 @@ PRODUCT_PACKAGES += \
     libtinycompress \
     sound_trigger.primary.sm6150
 
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 # Audio XML
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
